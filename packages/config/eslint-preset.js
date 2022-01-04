81eaa62b4ab1eb3b57cba58e75f904e346d43ff6
Varun S Athreya <varun.sathreya@gmail.com>
2022-01-05 02:50:34 +0530

    Finalized linitng packages

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,11 +1,35 @@
 module.exports = {
-  extends: ["next", "prettier"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/", "packages/*/"],
+    extends: [
+        "next",
+        "prettier",
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+    ],
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-  },
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint", "prettier"],
+    settings: {
+        next: {
+            rootDir: ["apps/*/", "packages/*/"],
+        },
+    },
+    rules: {
+        "@next/next/no-html-link-for-pages": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+    },
 };
