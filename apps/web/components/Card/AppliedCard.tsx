8c6bf56b133356da0fed7cda074d62cfcff38b05
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-01-09 19:42:32 +0530

    Made Stats & Applied Card

---
+++ apps/web/components/Card/AppliedCard.tsx
@@ -0,0 +1,67 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+const AppliedCard = (props) => {
+    const { name, email, branch, section, usn, company, type, ctc } = props;
+
+    return (
+        <Box p="15px" bg={'white'} my="8px" borderRadius="12px">
+            <Flex justify="space-between" w="100%" alignItems={'center'}>
+                <Flex direction="column" maxWidth="70%">
+                    <Text color={'black'} fontSize="lg" fontWeight="bold">
+                        {name}
+                    </Text>
+                    <Text color="gray.400" fontSize="sm" fontWeight="semibold">
+                        USN:{' '}
+                        <Text as="span" color="gray.500">
+                            {usn}
+                        </Text>
+                    </Text>
+                    <Text color="gray.400" fontSize="sm" fontWeight="semibold">
+                        Email Address:{' '}
+                        <Text as="span" color="gray.500">
+                            {email}
+                        </Text>
+                    </Text>
+                    <Text color="gray.400" fontSize="sm" fontWeight="semibold">
+                        Branch:{' '}
+                        <Text as="span" color="gray.500">
+                            {branch}
+                        </Text>
+                        {'  '}
+                        Sec:{' '}
+                        <Text as="span" color="gray.500">
+                            {section}
+                        </Text>
+                    </Text>
+                </Flex>
+                <Flex direction="column" maxWidth="70%">
+                    <Text color="gray.400" fontSize="sm" fontWeight="semibold">
+                        Company:{' '}
+                        <Text as="span" color="gray.500">
+                            {company}
+                        </Text>
+                    </Text>
+                    <Text color="gray.400" fontSize="sm" fontWeight="semibold">
+                        Type:{' '}
+                        <Text as="span" color="gray.500">
+                            {type}
+                        </Text>
+                    </Text>
+                    <Text color="gray.400" fontSize="sm" fontWeight="semibold">
+                        CTC:{' '}
+                        <Text as="span" color="gray.500">
+                            {ctc}
+                        </Text>
+                    </Text>
+                </Flex>
+                <Flex>
+                    <Text color={'black'} fontSize="lg" fontWeight="bold" m={4}>
+                        Applied
+                    </Text>
+                </Flex>
+            </Flex>
+        </Box>
+    );
+};
+
+export default AppliedCard;
---
+++ apps/web/components/Card/StatsCard.tsx
@@ -0,0 +1,35 @@
+import { Box, Flex, Stat, StatLabel, StatNumber } from '@chakra-ui/react';
+import IconBox from '../Icons/IconBox';
+import { WalletIcon } from '..//Icons/Icons.js';
+
+const StatsCard = () => {
+    return (
+        <Box minH="83px" bg="white" p={4} borderRadius={8}>
+            <Flex flexDirection="row" align="center" justify="space-evenly">
+                <Stat me="auto">
+                    <StatLabel
+                        fontSize="lg"
+                        color="gray.400"
+                        fontWeight="bold"
+                        pb=".1rem"
+                    >
+                        No of Students
+                    </StatLabel>
+                    <StatNumber fontSize="md" color="black">
+                        53,000
+                    </StatNumber>
+                </Stat>
+                <IconBox
+                    as="box"
+                    h={'45px'}
+                    w={'45px'}
+                    bg={'linear-gradient( 310deg, #7928CA 0%, #FF0080 100%)'}
+                >
+                    <WalletIcon h={'24px'} w={'24px'} color="white" />
+                </IconBox>
+            </Flex>
+        </Box>
+    );
+};
+
+export default StatsCard;
