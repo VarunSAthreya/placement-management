84c2fb23b5914ada671552b6686b41d8fe9592ac
Varun S Athreya <varun.sathreya@gmail.com>
2022-01-09 17:09:51 +0530

    Switched resolvers to take data from databse using prisma

--- apps/api/src/graphql/resolvers.ts
+++ apps/api/src/graphql/resolvers.ts
@@ -1,81 +1,101 @@
-import {
-    applied,
-    companies,
-    eligibility,
-    selected,
-    userDetails,
-    users,
-} from '../db';
+import { prisma } from '../db';
 
 export const resolvers = {
     Query: {
-        users: () => users,
-        user: (_: any, { USN }: { USN: string }) =>
-            users.find((student) => student.USN === USN.toUpperCase()),
-        companies: () => companies,
-        company: (_: any, { name }: { name: string }) =>
-            companies.find(
-                (company) => company.name.toLowerCase() === name.toLowerCase()
-            ),
-        applied: () => applied,
-        selected: () => selected,
-    },
-    User: {
-        details: (user: IUser) => {
-            user.details = userDetails.filter((std) => std.USN === user.USN)[0];
+        users: async () =>
+            prisma.user.findMany({
+                include: {
+                    details: {
+                        include: {
+                            applied: {
+                                include: {
+                                    user: true,
+                                    company: true,
+                                },
+                            },
+                            selected: {
+                                include: {
+                                    user: true,
+                                    company: true,
+                                },
+                            },
+                        },
+                    },
+                },
+            }),
+        user: async (_: any, { USN }: { USN: string }) =>
+            prisma.user.findUnique({
+                where: { USN },
+                include: {
+                    details: {
+                        include: {
+                            applied: {
+                                include: {
+                                    user: true,
+                                    company: true,
+                                },
+                            },
+                            selected: {
+                                include: {
+                                    user: true,
+                                    company: true,
+                                },
+                            },
+                        },
+                    },
+                },
+            }),
 
-            const appliedCompanies = applied.filter(
-                (app) => app.user === user.USN
-            );
-
-            user.details.applied = appliedCompanies.map((comp) => {
-                return {
-                    company: companies.filter(
-                        (company) => company.name === comp.company
-                    )[0],
-                };
-            });
-
-            const selectedCompanies = selected.filter(
-                (sle) => sle.user === user.USN
-            );
-
-            user.details.selected = selectedCompanies.map((comp) => {
-                return {
-                    company: companies.filter(
-                        (company) => company.name === comp.company
-                    )[0],
-                };
-            });
-
-            return user.details;
-        },
-    },
-    Company: {
-        applied: (company: ICompany) => {
-            const appliedStudentsUSN = applied.filter(
-                (app) => app.company === company.name
-            );
-
-            return appliedStudentsUSN.map((usn) => {
-                return {
-                    user: users.filter((std) => std.USN === usn.user)[0],
-                };
-            });
-        },
-        selected: (company: ICompany) => {
-            const selectedStudentsUSN = selected.filter(
-                (sle) => sle.company === company.name
-            );
-
-            return selectedStudentsUSN.map((usn) => {
-                return {
-                    user: users.filter((std) => std.USN === usn.user)[0],
-                };
-            });
-        },
-        eligibility: (company: ICompany) => {
-            return eligibility.filter((elg) => elg.name === company.name)[0];
-        },
+        companies: async () =>
+            prisma.company.findMany({
+                include: {
+                    eligibility: true,
+                    applied: {
+                        include: {
+                            user: true,
+                            company: true,
+                        },
+                    },
+                    selected: {
+                        include: {
+                            user: true,
+                            company: true,
+                        },
+                    },
+                },
+            }),
+        company: async (_: any, { name }: { name: string }) =>
+            prisma.company.findUnique({
+                where: { name },
+                include: {
+                    eligibility: true,
+                    applied: {
+                        include: {
+                            user: true,
+                            company: true,
+                        },
+                    },
+                    selected: {
+                        include: {
+                            user: true,
+                            company: true,
+                        },
+                    },
+                },
+            }),
+        applied: async () =>
+            prisma.applied.findMany({
+                include: {
+                    user: true,
+                    company: true,
+                },
+            }),
+        selected: () =>
+            prisma.selected.findMany({
+                include: {
+                    user: true,
+                    company: true,
+                },
+            }),
     },
 };
