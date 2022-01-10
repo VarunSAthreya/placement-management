331952c344bf8354c433e2c30e7ee690129558d8
Varun S Athreya <varun.sathreya@gmail.com>
2022-01-10 12:53:19 +0530

    Modularized schema

---
+++ apps/api/src/graphql/schema/company.ts
@@ -0,0 +1,45 @@
+import { gql } from 'apollo-server';
+
+const company = gql`
+    type Company {
+        name: String!
+        arrival_date: String
+        package: Float
+        type: CompanyType!
+        eligibility: CompanyEdibility!
+        applied: [Applied]!
+        selected: [Selected]!
+    }
+
+    enum CompanyType {
+        SERVICE
+        PRODUCT
+    }
+
+    type CompanyEdibility {
+        name: String!
+        CGPA: Float!
+        backlogs: Int!
+        tenth: Float!
+        twelth: Float!
+    }
+
+    input CompanyInput {
+        name: String!
+        arrival_date: String
+        package: Float
+        type: CompanyType!
+        eligibility: CompanyEdibilityInput!
+        applied: [AppliedInput]
+        selected: [SelectedInput]
+    }
+
+    input CompanyEdibilityInput {
+        CGPA: Float!
+        backlogs: Int!
+        tenth: Float!
+        twelth: Float!
+    }
+`;
+
+export default company;
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from 'apollo-server';
 import dotenv from 'dotenv';
 import { resolvers } from './graphql/resolvers';
-import { typeDefs } from './graphql/schema';
+import typeDefs from './graphql/schema';
 dotenv.config();
 
 const server = new ApolloServer({ typeDefs, resolvers });
