d5dd78a2c34c24f28373710898c3a1aea201cd78
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-01-10 19:57:53 +0530

    Made Company Applied Card

---
+++ apps/web/components/Card/CompanyAppliedCard.tsx
@@ -0,0 +1,46 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+const CompanyAppliedCard = (props) => {
+    const { company, type, ctc } = props;
+
+    return (
+        <Box p="15px" bg={'#f8f9fa'} my="8px" borderRadius="12px">
+            <Flex justify="space-between" w="100%" alignItems={'center'}>
+                <Flex direction="column" maxWidth="70%">
+                    <Text color={'black'} fontSize="lg" fontWeight="bold">
+                        {company}
+                    </Text>
+                    <Text color="gray.400" fontSize="sm" fontWeight="semibold">
+                        Type:{' '}
+                        <Text as="span" color="gray.500">
+                            {type}
+                        </Text>
+                    </Text>
+                    <Text color="gray.400" fontSize="sm" fontWeight="semibold">
+                        CTC:{' '}
+                        <Text as="span" color="gray.500">
+                            {ctc}
+                        </Text>
+                    </Text>
+                </Flex>
+                <Flex>
+                    <Text
+                        color={'white'}
+                        fontSize="md"
+                        fontWeight="bold"
+                        m={4}
+                        p={2}
+                        borderRadius={8}
+                        bg={
+                            'linear-gradient( 310deg, #7928CA 0%, #FF0080 100%)'
+                        }
+                    >
+                        Applied
+                    </Text>
+                </Flex>
+            </Flex>
+        </Box>
+    );
+};
+
+export default CompanyAppliedCard;
