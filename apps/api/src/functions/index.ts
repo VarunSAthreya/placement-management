d1883e67937ae5ac5655a3575dbeb4da5c06a842
Varun S Athreya <varun.sathreya@gmail.com>
2022-01-14 03:34:08 +0530

    Added verify token function

--- apps/api/src/functions/index.ts
+++ apps/api/src/functions/index.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 
 export const issueToken = async (user: IUser) => {
     const { USN, role, details } = user;
@@ -7,6 +7,19 @@ export const issueToken = async (user: IUser) => {
         expiresIn: 60 * 60 * 24 * 30,
     });
     return `Bearer ${token}`;
+};
+
+export const verifyToken = async (token: string) => {
+    if (!token || token === '') {
+        return null;
+    }
+
+    token = token.split(' ')[1];
+    if (!token || token === '') {
+        return null;
+    }
+
+    return verify(token, process.env.JWT_SECRET!);
 };
 
 export const encryptPassword = async (password: string) =>
