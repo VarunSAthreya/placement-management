3ad3b17265486a20b082c7bfbe8d2bff541f2c85
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-01-15 19:19:49 +0530

    Added Input Fields & Validation

--- apps/web/pages/login.tsx
+++ apps/web/pages/login.tsx
@@ -8,10 +8,23 @@ import {
     Icon,
     InputGroup,
     InputLeftElement,
+    Grid,
+    GridItem,
+    FormErrorMessage,
+    useColorModeValue,
 } from '@chakra-ui/react';
 import { AiOutlineMail, AiFillLock } from 'react-icons/ai';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    };
     return (
         <Flex position="relative" mb="40px">
             <Flex
@@ -30,89 +43,178 @@ const Login = () => {
                     p="32px"
                     mt={{ md: '150px', lg: '100px' }}
                     justifyContent={'center'}
-                    color="white"
+                    color="black"
                     borderRadius={8}
-                    bg={'linear-gradient( 310deg, #7928CA 0%, #FF0080 100%)'}
+                    bg={useColorModeValue('#f8f9fa', '#18191A')}
                 >
                     <Heading
                         fontSize="48px"
                         mb="20px"
                         p={1}
+                        color={useColorModeValue('black', 'white')}
                         textAlign={'center'}
                     >
                         Placement Portal
                     </Heading>
-                    <FormControl p={4}>
-                        <FormLabel ms="4px" fontSize="md" fontWeight="normal">
-                            Email
-                        </FormLabel>
-                        <InputGroup>
-                            <InputLeftElement pointerEvents="none">
-                                <Icon
-                                    as={AiOutlineMail}
-                                    color="black"
-                                    w={6}
-                                    h={6}
-                                    mt={'3px'}
-                                />
-                            </InputLeftElement>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Grid templateColumns="1fr">
+                            <GridItem>
+                                <FormControl isInvalid={errors.Email}>
+                                    <FormLabel
+                                        ms="4px"
+                                        fontSize="md"
+                                        fontWeight="normal"
+                                        color={useColorModeValue(
+                                            'black',
+                                            'white'
+                                        )}
+                                    >
+                                        Email
+                                    </FormLabel>
+                                    <InputGroup>
+                                        <InputLeftElement pointerEvents="none">
+                                            <Icon
+                                                as={AiOutlineMail}
+                                                color={useColorModeValue(
+                                                    'black',
+                                                    'white'
+                                                )}
+                                                w={6}
+                                                h={6}
+                                                mt={'3px'}
+                                            />
+                                        </InputLeftElement>
 
-                            <Input
-                                borderRadius="15px"
-                                variant={'filled'}
-                                mb="24px"
-                                focusBorderColor="black"
-                                _placeholder={{ color: 'black' }}
-                                fontSize="md"
-                                color="white"
-                                type="text"
-                                placeholder="Your email adress"
-                                size="lg"
-                            />
-                        </InputGroup>
-
-                        <FormLabel ms="4px" fontSize="md" fontWeight="normal">
-                            Password
-                        </FormLabel>
-                        <InputGroup>
-                            <InputLeftElement pointerEvents="none">
-                                <Icon
-                                    as={AiFillLock}
-                                    color="black"
-                                    w={6}
-                                    h={6}
-                                    mt={'3px'}
-                                />
-                            </InputLeftElement>
-                            <Input
-                                borderRadius="15px"
-                                variant={'filled'}
-                                mb="24px"
-                                fontSize="md"
-                                focusBorderColor="black"
-                                color="white"
-                                type="password"
-                                placeholder="Password"
-                                _placeholder={{ color: 'black' }}
-                                size="lg"
-                            />
-                        </InputGroup>
-                        <Button
-                            fontSize="10px"
-                            type="submit"
-                            bg="white"
-                            w="100%"
-                            h="45"
-                            mb="20px"
-                            color="black"
-                            mt="20px"
-                            _hover={{
-                                bg: '#e2e2e2',
-                            }}
-                        >
-                            SIGN IN
-                        </Button>
-                    </FormControl>
+                                        <Input
+                                            borderRadius="15px"
+                                            variant={'filled'}
+                                            mb="4px"
+                                            focusBorderColor={useColorModeValue(
+                                                'black',
+                                                'white'
+                                            )}
+                                            _placeholder={{
+                                                color: useColorModeValue(
+                                                    'black',
+                                                    'white'
+                                                ),
+                                            }}
+                                            fontSize="md"
+                                            color={useColorModeValue(
+                                                'black',
+                                                'white'
+                                            )}
+                                            type="text"
+                                            placeholder="Your email address"
+                                            size="lg"
+                                            {...register('Email', {
+                                                required:
+                                                    'Please Enter Your Email Address',
+                                                pattern: {
+                                                    value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                                    message:
+                                                        'Please Enter a Valid Email Address',
+                                                },
+                                            })}
+                                        />
+                                    </InputGroup>
+                                    <FormErrorMessage color="red">
+                                        {errors.Email && errors.Email.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                            </GridItem>
+                            <GridItem mt={4}>
+                                <FormControl isInvalid={errors.Password}>
+                                    <FormLabel
+                                        ms="4px"
+                                        fontSize="md"
+                                        fontWeight="normal"
+                                        color={useColorModeValue(
+                                            'black',
+                                            'white'
+                                        )}
+                                    >
+                                        Password
+                                    </FormLabel>
+                                    <InputGroup>
+                                        <InputLeftElement pointerEvents="none">
+                                            <Icon
+                                                as={AiFillLock}
+                                                color={useColorModeValue(
+                                                    'black',
+                                                    'white'
+                                                )}
+                                                w={6}
+                                                h={6}
+                                                mt={'3px'}
+                                            />
+                                        </InputLeftElement>
+                                        <Input
+                                            borderRadius="15px"
+                                            variant={'filled'}
+                                            mb="4px"
+                                            fontSize="md"
+                                            focusBorderColor={useColorModeValue(
+                                                'black',
+                                                'white'
+                                            )}
+                                            color={useColorModeValue(
+                                                'black',
+                                                'white'
+                                            )}
+                                            type="password"
+                                            placeholder="Password"
+                                            _placeholder={{
+                                                color: useColorModeValue(
+                                                    'black',
+                                                    'white'
+                                                ),
+                                            }}
+                                            size="lg"
+                                            {...register('Password', {
+                                                required:
+                                                    'Please Enter Valid Password',
+                                                minLength: {
+                                                    value: 3,
+                                                    message:
+                                                        'Minimum Password length should be 3',
+                                                },
+                                                maxLength: {
+                                                    value: 8,
+                                                    message:
+                                                        'Maximum Password length should be 8',
+                                                },
+                                            })}
+                                        />
+                                    </InputGroup>
+                                    <FormErrorMessage color="red">
+                                        {errors.Password &&
+                                            errors.Password.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                            </GridItem>
+                            <GridItem>
+                                <Button
+                                    fontSize="10px"
+                                    type="submit"
+                                    bg={useColorModeValue('black', 'white')}
+                                    w="100%"
+                                    h="45"
+                                    mb="20px"
+                                    color={useColorModeValue('white', 'black')}
+                                    mt="20px"
+                                    _hover={{
+                                        bg: useColorModeValue(
+                                            '#000000e0',
+                                            '#e2e2e2'
+                                        ),
+                                    }}
+                                >
+                                    SIGN IN
+                                </Button>
+                            </GridItem>
+                        </Grid>
+                    </form>
                 </Flex>
             </Flex>
         </Flex>
